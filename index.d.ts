273125d1a1786614466c2f19157850c71f6ea6be
Rijkaard Orismé <rijkaardorisme@gmail.com>
2018-04-02 19:30:08 -0400

    Add project files

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export namespace RemoteMethod {
+  export function modelsWithScopeRules(): Array<string>;
+  export function applyModelScopeRules(modelName: string): void;
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const config = require('../../model-config');
+
+const protos = [
+  'prototype.__count__',
+  'prototype.__create__',
+  'prototype.__delete__',
+  'prototype.__destroyById__',
+  'prototype.__findById__',
+  'prototype.__get__',
+  'prototype.__updateById__',
+];
+
+module.exports = {
+  RemoteMethod: {
+    modelsWithScopeRules: function() {
+      let models = Object.keys(config).filter((key) => {
+        if (key === '_meta') return;
+        if (!config[key].options) return;
+        if (!config[key].options.remoting) return;
+        if (!config[key].options.remoting.scopes) return;
+        return key;
+      });
+      return models;
+    },
+    applyModelScopeRules: function(lbModels) {
+      const methodsNames = [];
+      let models = this.modelsWithScopeRules();
+      models.forEach((modelName) => {
+        let model = config[modelName];
+        let scopeDefinitions = Object.keys(model.options.remoting.scopes);
+        scopeDefinitions.forEach((scope) => {
+          let definition = model.options.remoting.scopes[scope];
+          let methods = Object.keys(definition);
+          let disabledMethods = [];
+          methods.forEach((item, index) => {
+            if (item === '*') {
+              if (index > 0) console.warn(`Wildcard (*) rule must be declared before any other rules.`);
+              if (!definition[item]) protos.map(proto => disabledMethods.push(`${proto}${scope}`));
+              return;
+            }
+
+            let methodName = `prototype.__${item}__${scope}`;
+            if (disabledMethods.length === 0) {
+              disabledMethods.push(methodName);
+              return;
+            } else {
+              let index = disabledMethods.indexOf(methodName);
+              if (!definition[item] && index < 0) disabledMethods.push(methodName);
+              if (definition[item] && index > 0) disabledMethods = disabledMethods
+                .filter(method => method !== methodName);
+            }
+          });
+
+          disabledMethods.map((methodName) => {
+            lbModels[modelName].disableRemoteMethodByName(methodName)
+          });
+        });
+      });
+    }
+  },
+};
